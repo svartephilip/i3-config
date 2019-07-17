b68e1fa937522573ca1231c76b9b24a38c2f3847
oskar Haukebøe <ohaukeboe@gmail.com>
2019-07-17 20:26:38 +0200

    Automatic commit at: 17/07/2019

--- Desktop/minecraft/abserver/server4/paper-run.sh
+++ Desktop/minecraft/abserver/server4/paper-run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+VERSION=${1}
 ~/scripts/push-to-git.sh
+rm paperclip.jar
+wget https://papermc.io/ci/job/Paper-$VERSION/lastSuccessfulBuild/artifact/paperclip.jar
 sudo nice -n -10 java -jar paper*.jar
