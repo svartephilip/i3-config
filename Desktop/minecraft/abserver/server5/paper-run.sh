5e0729569bf989ed2ae551b63ebdc1398b076de1
oskar Haukebøe <ohaukeboe@gmail.com>
2019-09-27 13:27:07 +0200

    Automatic commit at: 27/09/2019
    Former-commit-id: 6592ba7dc75b70cf214bc1c5044167fb60aec68a

--- Desktop/minecraft/abserver/server5/paper-run.sh
+++ Desktop/minecraft/abserver/server5/paper-run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo url="https://www.duckdns.org/update?domains=minecraft-svartephilip&token=77d6573d-c968-498c-813a-618905567044&ip=" | curl -k -o ~/duckdns/duck.log -K -
 VERSION=${1:-1.14}
 ~/scripts/push-to-git.sh
 sudo rm paperclip.jar
