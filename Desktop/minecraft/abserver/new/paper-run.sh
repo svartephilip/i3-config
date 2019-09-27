9e57306be2295374fb6f87f626c3509fbd16498f
oskar Haukebøe <ohaukeboe@gmail.com>
2019-09-27 16:18:52 +0200

    Automatic commit at: 27/09/2019
    Former-commit-id: 454c2e057d720098e9153d97606e401bbc5a7262

--- Desktop/minecraft/abserver/new/paper-run.sh
+++ Desktop/minecraft/abserver/new/paper-run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo url="https://www.duckdns.org/update?domains=minecraft-svartephilip&token=77d6573d-c968-498c-813a-618905567044&ip=" | curl -k -o ~/duckdns/duck.log -K -
+ip=$(hostname -i)
+echo url="https://www.duckdns.org/update?domains=minecraft-svartephilip&token=77d6573d-c968-498c-813a-618905567044&ip=$ip" | curl -k -o ~/duckdns/duck.log -K -
 VERSION=${1:-1.14}
 ~/scripts/push-to-git.sh
 sudo rm paperclip.jar
--- Desktop/minecraft/abserver/new/start.sh
+++ Desktop/minecraft/abserver/new/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-echo url="https://www.duckdns.org/update?domains=minecraft-svartephilip&token=77d6573d-c968-498c-813a-618905567044&ip=" | curl -k -o ~/duckdns/duck.log -K -
+ip=$(hostname -i)
+echo url="https://www.duckdns.org/update?domains=minecraft-svartephilip&token=77d6573d-c968-498c-813a-618905567044&ip=$ip" | curl -k -o ~/duckdns/duck.log -K -
 ~/scripts/push-to-git.sh
 sudo nice -n -10 java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot*.jar
